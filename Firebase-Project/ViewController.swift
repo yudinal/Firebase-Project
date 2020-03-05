4f78da01446792f07e89eba9848f00ed4ae629a5
Lilia Yudina <liliayudina@gmail.com>
2020-03-05 17:34:21 -0500

    Initial Commit

---
+++ Firebase-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Project
+//
+//  Created by Lilia Yudina on 3/5/20.
+//  Copyright © 2020 Lilia Yudina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
